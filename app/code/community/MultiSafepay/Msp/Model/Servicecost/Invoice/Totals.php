1266dc10b0205ce28dc2c8829d87c5c5b64525d3
Hans Kuijpers <info@hkweb.nl>
2018-07-11 14:48:40 +0200

    update to 2.2.6

--- app/code/community/MultiSafepay/Msp/Model/Servicecost/Invoice/Totals.php
+++ app/code/community/MultiSafepay/Msp/Model/Servicecost/Invoice/Totals.php
@@ -7,17 +7,18 @@ class MultiSafepay_Msp_Model_Servicecost_Invoice_Totals extends Mage_Sales_Model
         $invoice->setServicecost($order->getServicecost());
         $invoice->setBaseServicecost($order->getBaseServicecost());
         $invoice->setServicecostTax($order->getServicecostTax());
+
         $invoice->setBaseServicecostTax($order->getBaseServicecostTax());
-        $invoice->setBaseTaxAmount($order->getBaseTaxAmount()+$order->getBaseServicecostTax());
-        $invoice->setTaxAmount($order->getTaxAmount()+$order->getBaseServicecostTax());
-        
 
-        $invoice->setBaseGrandTotal($order->getBaseGrandTotal()+$order->getBaseServicecostTax());
-        $invoice->setGrandTotal($order->getGrandTotal()+$order->getBaseServicecostTax());
-        $invoice->setSubtotalInclTax($order->getSubtotalInclTax());
-        $invoice->setBaseSubtotalInclTax($order->getBaseSubtotalInclTax());
+        $invoice->setBaseTaxAmount  ($order->getBaseTaxAmount()+$order->getBaseServicecostTax());
+        $invoice->setTaxAmount      ($order->getTaxAmount()+$order->getServicecostTax());
+
+        $invoice->setBaseGrandTotal($invoice->getBaseGrandTotal()+$order->getBaseServicecost());
+        $invoice->setGrandTotal    ($invoice->getGrandTotal()+$order->getServicecost());
+
+        $invoice->setSubtotalInclTax($invoice->getSubtotalInclTax());
+        $invoice->setBaseSubtotalInclTax($invoice->getBaseSubtotalInclTax());
         $invoice->setServicecostPdf($order->getServicecostPdf());
-
 
         //Magento will get the totalpaid amount and add the invoiced amount and set the totalpaid to the new value. This results in a double totalPaid value within 		//the order view. This happens only when auto creation of the invoice is disabled. To fix this we will set the Total Paid to 0 before the invoice is created 		//and the totalpaid is update again with the total invoiced.
         //$order->setTotalPaid(0);
